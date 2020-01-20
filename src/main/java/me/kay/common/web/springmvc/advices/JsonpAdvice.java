777b221a071c30a4b86a839b18c1b8a49728c3ce
kay <kayfhan@sina.com>
2020-01-20 20:32:03 +0800

    first commit

---
+++ src/main/java/me/kay/common/web/springmvc/advices/JsonpAdvice.java
@@ -0,0 +1,13 @@
+package me.kay.common.web.springmvc.advices;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by zhouxiaoming on 2015/9/17.
+ */
+@ControllerAdvice
+public class JsonpAdvice extends FastJsonJsonpResponseBodyAdvice {
+    public JsonpAdvice() {
+        super("callback");
+    }
+}
